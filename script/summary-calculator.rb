36ef3d7d2ca3c79d3fc0ab5d4302ca8e96ae3109
John Murphy <murphybytes@gmail.com>
2012-10-18 21:04:50 -0500

    updated summary calculator with better params and fixed kw calculations

--- script/summary-calculator.rb
+++ script/summary-calculator.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
-1$: << File.join( Dir.pwd, 'lib' )
+$: << File.join( Dir.pwd, 'lib' )
 require 'date'
 require 'mongoid'
-require 'trollop'
 require 'models/sample'
 require 'models/data_set'
+require 'ostruct'
+require 'optparse'
 
-POWER_UNIT_TO_KW_CONVERSION=19.5 * 1000
 HOME_TEMP=70.0
 
 class Calculator
@@ -15,41 +15,68 @@ class Calculator
     @opts = opts
     @total_kw_generated = 0.0
     @total_kw_required_for_heating = 0.0
-    @base_temp = opts[:base_temperature] 
-    @number_of_homes =  opts[:number_of_homes] 
+    @base_temp = opts.base_temperature
+    @number_of_homes =  opts.home_count
   end
 
   def process_sample( sample )
-    @total_kw_generated += sample.power_units
-    
+    @total_kw_generated += sample.generated_kilowatts
+
     if @base_temp > sample.temperature
       @total_kw_required_for_heating += (HOME_TEMP - sample.temperature) 
     end
   end
 
   def show_results
-    puts "Total KW Generated #{(@total_kw_generated * POWER_UNIT_TO_KW_CONVERSION).to_i }"
+    puts "Total KW Generated #{@total_kw_generated}"
     puts "Total KW Required for Home Heating #{ (@total_kw_required_for_heating * 713.4889 * @number_of_homes / 3413.0 ).to_i }" 
   end
 
 end
 
-opts = Trollop::options do 
-  opt :data_set_name, "Name of data set to contain populations", :type => :string
-  opt :environment, "development | test | production", :type => :string, :default => 'development'
-  opt :base_temperature, "If outside temperature drops below base temperature (F) home heating is calculated", :default => 60
-  opt :number_of_homes, "Number of homes used to calculate Kw load", :default => 1000
-  #opt :sample_column, "Column of the csv to process", :type => :integer
+options = OpenStruct.new
+options.data_set_name = nil
+options.environment = "development"
+options.base_temperature = 60
+options.home_count = 1000
+
+opts = OptionParser.new do | opts |
+  opts.banner = "Usage: summary-calculator [options]"
+  opts.separator ""
+  opts.separator "Options:"
+
+  opts.on( "-d", "--data-set-name VALUE", "Name of data set to contain populations" ) do | value |
+    options.data_set_name = value
+  end
+
+  opts.on( "-e", "--environment VALUE", "Default = #{options.environment}" ) do | value |
+    options.environment = value
+  end
+
+  opts.on( "-b", "--base-temparature VALUE",  "If outside temperature drops below base temperature (F) home heating is calculated. Default = #{options.base_temperature}" ) do | value |
+    options.base_temperature = value
+  end
+
+  opts.on( "-n", "--home-count VALUE", "Number of homes used to calculate Kw load. Default = #{options.home_count}" ) do | value |
+    options.home_count = value
+  end
+
+  opts.on_tail( "-h", "--help", "Show this message" ) do 
+    puts opts
+    exit
+  end
 end
 
 begin
+  opts.parse!(ARGV)
 
-  Mongoid.load!( 'config/mongoid.yml', opts[:environment].to_sym )
+  Mongoid.load!( 'config/mongoid.yml', options.environment.to_sym )
   Mongoid.raise_not_found_error = false
+  raise "Missing required data set name" if options.data_set_name.nil?
 
-  dataset = DataSet.where( name: opts[:data_set_name] )
- 
-  calculator = Calculator.new opts 
+  dataset = DataSet.where( name: options.data_set_name )
+
+  calculator = Calculator.new options
 
   puts "Working"
 
@@ -62,6 +89,7 @@ begin
   calculator.show_results
 
 rescue => e
-  puts "Program failed #{ e }"
+  puts "Program failed -> #{ e }"
   puts e.backtrace.join("\n")
+  exit 1
 end
