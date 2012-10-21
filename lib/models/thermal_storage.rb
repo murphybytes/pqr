09458954973e7cbd8a31370d4d9a9e37677c0f86
John Murphy <murphybytes@gmail.com>
2012-10-21 10:06:44 -0500

    added additional factor to thermal charges

--- lib/models/thermal_storage.rb
+++ lib/models/thermal_storage.rb
@@ -1,5 +1,15 @@
-
-
+#####################################################################
+# Describes a class of thermal storage
+#
+# units          - the number of devices, i.e. 500 water heaters
+# capacity       - max kW storage for each device
+# base_threshold - if stored kW falls below this value we must charge
+# storage        - average kW stored in device
+# charge_rate    - kW/hr that we can replenish storage
+# name           - title of thermal storage
+# usage          - reduction of storage due to usage, cooling
+#                  in kW per hour
+#####################################################################
 class ThermalStorage
   include Mongoid::Document
   belongs_to :data_set
@@ -12,14 +22,14 @@ class ThermalStorage
   field :storage, type: BigDecimal
   field :charge_rate, type: BigDecimal
   field :name, type: String
-
+  field :usage, type: BigDecimal
 
   index( {name: 1}, {  name: 'name_index' } )
 
   def self.fetch_by_dataset( dataset, name ) 
     results = []
     DataSet.where( name: dataset ).each do | ds | 
-      ThermalStorage.where( data_set_id: ds.id, name: name ).each do | ts | 
+      ThermalStorage.where( data_set_id: gds.id, name: name ).each do | ts | 
         results << ts
         yield ts 
       end
@@ -36,6 +46,7 @@ class ThermalStorage
     puts "Base Threshold (kW): #{ self.base_threshold }"
     puts "Storage (kW): #{ self.storage }"
     puts "Charge Rate (kW/hr): #{ self.charge_rate }"
+    puts "Usage (kW/hr): #{ self.usage }"
     puts  ""
   end
 
